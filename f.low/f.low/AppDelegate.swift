67bd76e73d6040734317c82fcf7191073464a4c1
= <owenkundev@gmail.com>
2016-02-20 14:14:21 -0500

    add skeleton

---
+++ f.low/f.low/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  f.low
+//
+//  Created by Owen Li on 2016-02-20.
+//  Copyright © 2016 Owen Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
